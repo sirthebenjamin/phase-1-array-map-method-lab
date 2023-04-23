53dedd099e7215f20956f9264cc4ae62f7e2f0bd
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-04-22 21:26:46 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ')
+                   .map(word => {
+                      return word.charAt(0).toUpperCase() + word.slice(1);
+                    })
+                   .join(' ');
+  });
 }
+
+console.log(titleCased());
+// Output: [
+//   "What Does The This Keyword Mean?",
+//   "What Is The Constructor OO Pattern?",
+//   "Implementing Blockchain Web API",
+//   "The Test Driven Development Workflow",
+//   "What Is NaN And How Can We Check For It",
+//   "What Is The Difference Between StopPropagation And PreventDefault?",
+//   "Immutable State And Pure Functions",
+//   "What Is The Difference Between == And ===?",
+//   "What Is The Difference Between Event Capturing And Bubbling?",
+//   "What Is JSONP?"
+// ]
